d314e5ff29a18ff7c3230b4c98232860fb82ee68
pankaj3571 <53375656+pankaj3571@users.noreply.github.com>
2020-03-28 09:15:42 +0530

    add password field

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,10 +13,12 @@ export class LoginComponent implements OnInit {
   username:any;
   mobile:any
   value:any;
+  password:any;
   obj:any;
   constructor(private router:Router,private service:CommonService) { }
 
   ngOnInit() {
+    this.getData()
   }
 
   move(event){
@@ -43,12 +45,20 @@ export class LoginComponent implements OnInit {
     this.obj={
       email:this.email,
       username:this.username,
-      mobile:this.mobile
+      mobile:this.mobile,
+      password:this.password
     }
     this.service.registerData(this.obj).subscribe(data=>{
       console.log(data)
+      alert(data['message'])
     })
 
+  }
+
+  getData(){
+    this.service.getData().subscribe(data=>{
+      console.log(data)
+    })
   }
 }
 
